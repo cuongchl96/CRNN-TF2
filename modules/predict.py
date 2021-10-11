f81bf87fe5ffb7ee2d3d4a5b26104ba111bd1630
Cương Lê Trần Bảo <cuongltb@kalapa.vn>
2021-10-11 03:28:29 +0000

    add predict folder

--- modules/predict.py
+++ modules/predict.py
@@ -6,6 +6,7 @@ import logging
 import tensorflow as tf
 import argparse
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import numpy as np
 import cv2
 
@@ -16,6 +17,7 @@ from modules.parameters.base_config import Config
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 tf.config.experimental.enable_tensor_float_32_execution(False)
+tf.get_logger().setLevel("ERROR")
 
 def evaluate(infer_fn, opt):
     generator = Dataset(opt.dataset.test_set, epochs=1, text_converter=text_converter, image_converter=image_converter)
@@ -56,12 +58,22 @@ def evaluate_case(infer_fn, opt):
 def predict(infer_fn, image_path, opt):
     image = cv2.imread(image_path)
     processed_image = tf.convert_to_tensor([image_converter(image)])
-    text_for_pred = tf.zeros([1, opt.model_params.max_len + 1], dtype=tf.int32)
+    text_for_pred = tf.zeros([1, 21], dtype=tf.int32)
 
     logits = infer_fn(processed_image, text_for_pred)['tf.compat.v1.transpose']
     logits = tf.argmax(logits, axis=2)
 
     return text_converter.decode(logits)
+
+def predict_folder(infer_fn, folder, opt, plot_image=False):
+    import matplotlib.pyplot as plt
+
+    for fn in os.listdir(folder):
+        text = predict(infer_fn, os.path.join(folder, fn), opt)
+        print(fn, text)
+        plt.imshow(cv2.imread(os.path.join(folder, fn))[..., ::-1])
+        plt.show()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -70,6 +82,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     opt = Config.from_yaml(args.config_file)
+    print(opt)
 
     text_converter = AttnLabelConverter(opt)
     image_converter = NormalizePAD(opt, is_training=False)
@@ -81,5 +94,6 @@ if __name__ == "__main__":
     infer._num_positional_args = 2
 
     # evaluate(infer, opt)
-    evaluate_case(infer, opt)
+    # evaluate_case(infer, opt)
     # predict(infer, '/media/cuongvc/UBUNTU/home/cuongvc/cuongltb/aocr/datasets/idcards/idnum/image/2020-02-03-15-28-38_211442911_VO-THI-CHANH_True.jpg', opt)
+    predict_folder(infer, '/workspace/TensorFlow/workspace/passport_text_detection/pieces/generic', True)
